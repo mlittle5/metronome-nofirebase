0b9eb5f8cf9c1cc51607d2b388a29eb9a1f5b6f2
Morgan Little <mrgnlit@gmail.com>
2020-12-06 22:14:00 -0800

    doing some experimentation with react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, Component } from "react";
+
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import { Container, Row, Col, Jumbotron, Media } from "react-bootstrap";
+import pizzas from "./data.json";
+import { PizzaCard } from "./components/PizzaCard";
+import { Confirmation } from "./components/Comfirmation";
+import Tree from "./components/christmas_penis.png";
+import RainbowText from "react-rainbow-text";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Jumbotron>
+      <Media>
+        <Media.Body>
+          <h1 className="display-2">Hello world!</h1>
+          <p className="lead">This is a simple jumbotron</p>
+          <hr className="my-4" />
+          <p>
+            this is a sweet pic of a guy with a christmas tree as his penis.
+          </p>
+          <hr className="my-2" />
+          <br />
+          <br />
+          <h4 className="display-4">
+            <RainbowText lightness={0.5} saturation={1}>
+              Happy Holidays!
+            </RainbowText>
+          </h4>
+        </Media.Body>
+        <img src={Tree} className="ml-3" alt="headshot" />
+      </Media>
+    </Jumbotron>
   );
 }
 
